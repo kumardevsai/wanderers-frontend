9e09afe6d7ff9665704dff96cd61778004391029
Marian Serna <marianhalliday@gmail.com>
2017-10-29 23:06:09 -0400

    Moving map into its own component

--- src/pages/Places.jsx
+++ src/pages/Places.jsx
@@ -1,23 +1,11 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
-import ReactMapGL from 'react-map-gl';
+import MapGL from '../components/MapGL';
 
 @inject('WanderersStore')
 @observer
 export default class Places extends Component {
   render() {
-    const { WanderersStore } = this.props;
-    const viewport = WanderersStore.viewport;
-
-    return (
-      <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken="pk.eyJ1IjoibWFyaWFuc2VybmEiLCJhIjoiY2o0dm8wcGpqMHZ2YzJxcjV0ZDFvbTM5dSJ9.W5BkzLIaUIZcVuiSFbVTsw"
-        onViewportChange={viewport => {
-          WanderersStore.viewport = viewport;
-        }}
-        mapStyle="mapbox://styles/marianserna/cj9dl49je6ab72smd6pt33qwh"
-      />
-    );
+    return <MapGL />;
   }
 }
