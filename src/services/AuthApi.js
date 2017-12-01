0c32858c0ba5a70f37cdb399f84f6d13acc27bbe
Marian Serna <marianhalliday@gmail.com>
2017-11-30 23:54:01 -0500

    Adding back file with correct case

---
+++ src/services/AuthApi.js
@@ -0,0 +1,34 @@
+import { create } from 'apisauce';
+
+import Hosts from './Hosts';
+
+export default class AuthApi {
+  constructor() {
+    this.api = create({
+      // custom url (it has to redirect to facebook) -> atom /etc/hosts
+      baseURL: Hosts.authHost(),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    });
+  }
+
+  login = (email, password) => {
+    // post to #sessions from authentication-ms
+    return this.api.post('/sessions', {
+      email,
+      password
+    });
+  };
+
+  signup = (name, email, password, image) => {
+    const form_data = new FormData();
+    form_data.append('user[name]', name);
+    form_data.append('user[email]', email);
+    form_data.append('user[password]', password);
+    form_data.append('user[image]', image);
+    // post to #users from authentication-ms
+    return this.api.post('/users', form_data);
+  };
+}
